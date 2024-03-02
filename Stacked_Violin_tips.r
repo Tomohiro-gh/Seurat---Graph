041242e26651803bdc2ea9da48743b09f39e6e94
Tomohiro <52723738+Tomohiro-gh@users.noreply.github.com>
2024-03-02 11:51:51 +0900

    Update Stacked_Violin_tips.r

--- Stacked_Violin_tips.r
+++ Stacked_Violin_tips.r
@@ -2,6 +2,7 @@
 
 library(Seurat)
 library(scales)
+library(ggsci)
 
 ## get length
 Idents(seurat_obj) %>% unique() %>% length() #19
@@ -9,10 +10,18 @@ Idents(seurat_obj) %>% unique() %>% length() #19
 ## color に関して
 ## https://paulvanderlaken.com/2020/03/20/how-to-standardize-group-colors-in-data-visualizations-in-r/
 
-    show_col(hue_pal()(19))
+show_col(hue_pal()(19))
+## or 
 # color をvectorへ入れる
 myColors = hue_pal()(19)
 
+## またはggsciのカラーコードを使う場合，　例えばaaas colorにしてみると
+    library(ggsci)
+    show_col(pal_aaas("default")(10))
+myColors_ggsci = pal_aaas(palette = c("default"), alpha = 1)(10)
+
+
+## Identの変更
 Idents(seurat_obj) <- "CellAnnotation_Fine"
 
 
